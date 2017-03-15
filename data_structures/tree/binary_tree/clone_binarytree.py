e735d5352e01db486f8a42990e9b58ed6eb95b5b
System Administrator <root@LiZacks-MacBook-Pro.local>
2017-03-15 12:54:40 +0800

    submitted clone binary tree

---
+++ data_structures/tree/binary_tree/clone_binarytree.py
@@ -0,0 +1,46 @@
+## Reference
+# http://www.lintcode.com/en/problem/clone-binary-tree/
+
+## Tags - Easy
+# Binary Tree;
+
+
+## Description
+# For the given binary tree, return a DEEP COPY of it.
+
+
+## Analysis
+# input - @param: the root of a binary tree
+# output - @return root of the new tree
+# return the new binary tree with same structure and same value.
+# traverse and create new node
+# divide conquer? seems traverse is better than divide and conquer, as only root can be returned.
+# actuall divide and conquer is better 
+
+
+## Solution
+# definition of binary tree
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+class Solution:
+    def cloneTree(self, root):
+        if not root:
+            return None
+        # clone root value
+        newroot = TreeNode(root.val)
+        # clone left subtree
+        left = self.cloneTree(root.left)
+        # clone right subtree
+        right = self.cloneTree(root.right)
+        # merge left and right to root
+        newroot.left, newroot.right = left, right
+        return newroot
+
+
+## Solution
+
+
+
