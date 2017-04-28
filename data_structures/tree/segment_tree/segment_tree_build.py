771c91695e2043ab3208b47f638399804dd21fcb
System Administrator <root@LiZacks-MacBook-Pro.local>
2017-04-28 17:30:24 +0800

    corrected the typo

--- data_structures/tree/segment_tree/segment_tree_build.py
+++ data_structures/tree/segment_tree/segment_tree_build.py
@@ -1,27 +1,25 @@
 ## Reference
 # http://www.lintcode.com/en/problem/segment-tree-build/
 
-
 ## Tags - Medium
 # Binary Tree; Segment Tree
 
 ## Description
-# The structure of Segment Tree is a binary tree which each node has two attributes start and end denote
-# an segment/interval.
+# The structure of Segment Tree is a binary tree which each node has two attributes start and end,
+#  denote an segment/interval.
 # start and end are both integers, they should be assigned in following rules:
 # 1) The root's start and end is given by build method.
 # 2) The left child of node A has: start=A.left, end = (A.left + A.right) // 2
 # 3) The right child of node A has: start(A.left+A.right) // 2+1, end=A.right
 # 4) if start equals to end, there will be no children for this node. - BASE CASE
 # Implement a build method with two parameters start and end. 
-# So taht we can create a corresponding segment tree with every node has the correct start and end value
+# So that we can create a corresponding segment tree with every node has the correct start and end value
 # return the root of this segment tree.
 
 ## Clarification
 # Segment Tree (a.k.a Interval Tree) is an advanced data structure which can support queries like
 # 1) which of these intervals contain a given point
 # 2) which of these points are in a given interval
-
 
 ## Analysis
 # The segment tree is used to handle interval/range queries
@@ -33,17 +31,15 @@
 # 1) prepare the tree node.
 # 2) build root, left, and right
 
-
 ## Solution
 # segment tree node prepare
 class SegmentTreeNode:
     def __init__(self, start, end):
-        start, end = None, None
-        left, right = None, None
+        self.start, self.end = None, None
+        self.left, self.right = None, None
 
 # @Param start, end: Denote an segment/interval
 # @return: the root of Segment Tree
-
 class Solution:
     # recursion - divide and conquer
     def build(self, start, end):
@@ -59,6 +55,4 @@ class Solution:
         root.right = self.build((start_end)//2 + 1, end)
          
         return root
-
-
    
