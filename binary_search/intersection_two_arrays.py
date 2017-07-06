1088e4a249f3150f6905bb2e1bd70c0555a2ad78
System Administrator <root@LiZacks-MacBook-Pro.local>
2017-07-06 17:11:00 +0800

    updated two pointers solution

--- binary_search/intersection_two_arrays.py
+++ binary_search/intersection_two_arrays.py
@@ -1,8 +1,9 @@
 ## Reference
 # http://www.lintcode.com/en/problem/intersection-of-two-arrays/
+# 349 https://leetcode.com/problems/intersection-of-two-arrays/#/description
 
-## Tags
-# Easy - Binary Search; Two Pointers; Sort; Hash Table
+## Tags - Easy
+# Binary Search; Two Pointers; Sort; Hash Table
 
 ## Description
 # Given two arrays, write a function to compute their intersection.
@@ -24,17 +25,20 @@
 ## Solution
 class Solution:
     # python library - set
+    # 1) list to set; 
+    # 2) s.intersection(t)
+    # 3) set to list
     def intersection1(self, nums1, nums2):
         if not nums1 or not nums2:
             return []
         result = []
         nums1 = set(nums1)
         nums2 = set(nums2)
-        # sets operation returned sets !!!
+        # sets operation returned sets !
         # so we have to convert it from set to list
         return list(nums2.intersection(nums1))
 
-    def binarySearch(array, target):
+    def bsearch(self, array, target):
         if not array:
             return False
         N = len(array)
@@ -47,30 +51,26 @@ class Solution:
                 start = middle
             else:
                 end = middle
-        if array[start] == target:
-            return True
-        if array[end] == target:
+        if array[start] == target or array[end] == target:
             return True
         return False 
 
-    # bninary search
-    # O(MlogM+NlogN) time; O(M+N) time
+    # Sort + binary search
+    # O(MlogM+(M+N)logN) time; O(M+N) space
     def intersection2(self, nums1, nums2):
+        # corner case can improve efficiency
         if not nums1 or not nums2:
             return []
-        # init result
         result = []
-        # sort first ?
         nums1.sort()
         nums2.sort()
-        robot = 0
-        N = len(nums2)
-        while robot < N:
-            if robot + 1 < N and nums2[robot]
-        
-         
-       
-
+	M = len(nums2)
+        for i in xrange(M):
+            if i != 0 and nums2[i] == nums2[i-1]:
+                continue
+            if self.bsearch(nums1, nums2[i]):
+                result.append(nums2[i])
+        return result
 
     # hash table
     # O(M) + O(N) time; O(M)space
@@ -87,16 +87,41 @@ class Solution:
             if num in nums:
                 if nums[num] == 0:
                     result.append(num)
+                    # KEY - facility to avoid the duplicates included into the result
                     nums[num] = 1
         return result  
-                 
+       
+    # sort and merge + hash
+    # NlogN + N time; O(N)space 
+    def intersection4(self, nums1, nums2):
+        # corner case
+        if not nums1 or not nums2:
+            return []
+        nums1.sort()
+        nums2.sort()
+        N, M = len(nums1), len(nums2)
+        i, j = 0, 0
+        result = {}
+        while i < N and j < M:
+            while i < N and j < M and nums1[i] < nums2[j]:
+                i += 1
+            while i < N and j < M and nums2[j] < nums1[i]:
+                j += 1
+            if i < N and j < M and nums1[i] == nums2[j]:
+                if nums1[i] not in result:
+                    result[nums1[i]] = True
+                i += 1
+                j += 1
+        return result.keys()           
         
 if __name__ == "__main__":
     nums1 = [1, 2, 2, 1]
     nums2 = [2, 2]
     ans = Solution()
     if ans.intersection1(nums1, nums2) != [2] or \
-        ans.intersection3(nums1, nums2) != [2]:
-        print("test failed")
+        ans.intersection3(nums1, nums2) != [2] or \
+        ans.intersection4(nums1, nums2) != [2] or \
+        ans.intersection2(nums1, nums2) != [2]:
+        print("Failed: intersection of two arrays")
     else:
-        print("test passed")
+        print("Passed: intersection of two arrays")
