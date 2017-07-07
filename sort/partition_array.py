a87fbf58ff08775b64e619644be023843caab15e
System Administrator <root@LiZacks-MacBook-Pro.local>
2017-07-07 16:57:41 +0800

    implemented partition array

--- sort/partition_array.py
+++ sort/partition_array.py
@@ -1,10 +1,8 @@
 ## Reference
 # http://www.lintcode.com/en/problem/partition-array/#
 
-
 ## Tags - Medium
-# Quik Sort
-
+# Quik Sort; Two Pointers; Array
 
 ## Description
 # Given an array nums of integers and an int k, partition the array(i.e move the elements in "nums")
@@ -23,8 +21,35 @@
 # input - array of integers; int k
 # output None - the first index i that nums[i] >= k
 # corner case, if no item, return 0 according to the notice.
+# Two pointers
 
+## Solution
+class Solution:
+    def partitionArray(self, nums, k):
+        if not nums:
+	    return 0
+        N = len(nums)
+        start, end = 0, N-1
+        while start <= end:
+            while start <=end and nums[start] < k:
+                start += 1
+            while start <= end and nums[end] >= k:
+                end -= 1
+            if start <= end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        return start 
 
+if __name__ == "__main__":
+    nums = [3, 2, 2, 1]
+    k = 2
+    ans = Solution()
+    if ans.partitionArray(nums, k) == 1:
+        print("Passed: Partition Array.")
+    else:
+        print("Failed: Partition Array.")
+            
 
 
 
