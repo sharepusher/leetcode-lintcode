765be39f05e076130a97d59f0b8583175da4e43c
sharepusher <asmerada@hotmail.com>
2019-05-24 00:19:43 +0800

    two sum less than target

---
+++ two_pointers/two_sum_less_than_or_equal_target.py
@@ -0,0 +1,30 @@
+## Reference
+# https://www.lintcode.com/problem/two-sum-less-than-or-equal-to-target/description
+
+## Medium - Sort/TwoPointer
+
+## Description
+# Given an array of integers, find how many pairs in the array such that their sum is less than or equal to a specific target number. 
+# Please return the number of pairs.
+
+## Example
+# Input: nums = [2, 7, 11, 15], target = 24.
+# Output: 5.
+# Explanation:
+# 2 + 7 < 24
+# 2 + 11 < 24
+# 2 + 15 < 24
+# 7 + 11 < 24
+# 7 + 15 < 25
+
+# Input: nums = [1], target = 1. 
+# Output: 0. 
+
+## Solution
+# almost the same as Two-Sum-Greater_than_target
+# KEY: all the numbers between left and right <= target all the pair to be count.
+
+
+
+
+
