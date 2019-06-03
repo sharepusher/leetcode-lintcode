3550e7453bd9b241674224779283b697fab14445
sharepusher <asmerada@hotmail.com>
2019-06-04 00:15:27 +0800

    two sum design data structure

---
+++ data_structures/hashtable/two_sum3_data_structure_design.py
@@ -0,0 +1,21 @@
+## Reference
+# https://www.lintcode.com/problem/two-sum-iii-data-structure-design/description
+
+## Easy - Linkedin - DataStructureDesign/HashTable
+
+## Description
+# Design and implement a TwoSum class, which support add and find function.
+# add - Add the number to an internal data structure.
+# find - Find if there exists any pair of numbers which sum is equal to the value.
+
+## Example
+# add(1); add(3); add(5);
+# find(4) // return true
+# find(7) // return false
+
+## Solution
+# key: 1. hashtable for two sum
+#      2. handle duplicated item properly 
+
+
+
