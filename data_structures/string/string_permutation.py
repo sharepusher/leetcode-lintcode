55e56e4e501a758d700c303583202a41d1e894f5
sharepusher <asmerada@hotmail.com>
2019-07-16 22:16:06 +0800

    string permutation

---
+++ data_structures/string/string_permutation.py
@@ -0,0 +1,19 @@
+## Reference
+# https://www.lintcode.com/problem/string-permutation/description
+
+## Easy - Permutation/String
+
+## Description
+# Given two strings, write a function to decide if one is a permutation of the other.
+
+# Example
+# Example 1:
+#	Input:  "abcd", "bcad"
+#	Output:  True
+# Example 2:
+# 	Input: "aac", "abc"
+#	Output:  False
+
+
+
+
