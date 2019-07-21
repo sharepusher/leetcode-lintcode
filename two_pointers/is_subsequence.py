db525489afee2bfbbbfabb05f74ec8e59c60c9c1
sharepusher <asmerada@hotmail.com>
2019-07-21 22:12:10 +0800

    is subsequence

---
+++ two_pointers/is_subsequence.py
@@ -0,0 +1,30 @@
+## Reference
+# https://www.lintcode.com/problem/is-subsequence/description
+
+## Medium - Pintrest - TwoPointers/String
+
+## Description
+# Given a string s and a string t, check if s is a subseqence of t.
+# You may assume that there is only lower case English letters in both s and t.
+# t is potentially a very long(length~=50,0000) string, and s is a short string(length < 100).
+
+# A substring of a string is a new string which is formed from the original string by deleting some 
+# (can be none) of the characters without disturbing the relative positions of the remaining characters.
+# ie. "ace" is a subsequence of "abcde" while "aec" is not.
+
+## Example
+# Input: s = "abc", t = "ahbgdc"
+# Output: true
+# Input: s = `"axc"`, t = `"ahbgdc"`
+# Output: false
+
+## Challenge
+# If there are lots of incoming S1,S2....Sk where k>1B, and you want to check one by one to see 
+# if T has its subsequence.
+# In this senario, how would you change your code?
+
+
+
+
+
+
