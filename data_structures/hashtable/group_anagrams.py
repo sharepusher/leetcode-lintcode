fc63731914f3419aa08e792d143133b4adab1d71
sharepusher <asmerada@hotmail.com>
2019-08-05 21:56:58 +0800

    group anagrams

---
+++ data_structures/hashtable/group_anagrams.py
@@ -0,0 +1,26 @@
+## Reference
+# https://www.lintcode.com/problem/group-anagrams/description
+
+## Description
+# Given an array of strings, group anagrams together
+# NOTE: All inputs will be in lower-case
+
+## Example
+# Input:
+# ["eat","tea","tan","ate","nat","bat"]
+# Output:
+# [["ate","eat","tea"],
+#  ["bat"],
+#  ["nat","tan"]]
+#
+# Input:
+# ["eat","nowhere"]
+# Output:
+# [["eat"],
+#  ["nowhere"]]
+
+## Analysis
+# The same as Anagrams
+# group anagrams after sorted
+
+
