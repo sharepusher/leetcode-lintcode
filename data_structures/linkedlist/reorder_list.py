705614e6a09a905df89a4d33cdd1c18e47f75e42
sharepusher <asmerada@hotmail.com>
2019-10-16 23:26:21 +0800

    reorder list

---
+++ data_structures/linkedlist/reorder_list.py
@@ -0,0 +1,16 @@
+## Reference
+# https://www.lintcode.com/problem/reorder-list/description
+
+## Medium - linkedlist
+
+## Description
+# Given a singly linked list L: L0-> L1 -> ... -> LN-1 -> LN
+# reorder the list to: L0 -> LN -> L1 -> LN-1 -> L2 -> LN-2 ....
+
+## Example
+# Input: 1->2->3->4->null Output: 1->4->2->3->null
+# Input: 1->2->3->4->5->null Output: 1->5->2->4->3->null
+
+
+
+
